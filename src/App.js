24a59af2e81061399e2a3a4b58c8c385984a8762
tkrogers3 <tkrogers3@gmail.com>
2020-07-15 22:19:06 -0400

    Axios call brining back data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Header from './components/Header.js';
+import axios from 'axios';
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems();
+  }, [])
+
+
+
+
   return (
     <div className="container">
-     Breaking Bad Characters
+      <Header />
     </div>
   );
 }
