fe6b24828bbd17a20d0506201fd3ef325ba02111
tkrogers3 <tkrogers3@gmail.com>
2020-07-16 01:06:28 -0400

    Working grid. May potentially add more to the attributes of each character but calling this Deffinition of Done for now.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import Header from './components/Header.js';
+import CharacterGrid from './components/characters/CharacterGrid.js';
+import Search from './components/ui/Search.js'
 import axios from 'axios';
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
-      console.log(result.data)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+      // console.log(result.data)
       setItems(result.data)
       setIsLoading(false)
     }
     fetchItems();
-  }, [])
+  }, [query])
 
 
 
@@ -23,6 +26,11 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
+      <CharacterGrid 
+        isLoading = {isLoading}
+        items = {items}
+      />
     </div>
   );
 }
